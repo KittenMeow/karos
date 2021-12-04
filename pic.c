2279c5ed3ef3b8c3c3d81fb37b2ebc23a0a5bfa5
Cat <39655575+KittenMeow@users.noreply.github.com>
2021-12-04 17:18:16 -0500

    Add files via upload

---
+++ pic.c
@@ -0,0 +1,222 @@
+#define ICW1_ICW4	0x01		/* ICW4 (not) needed */
+#define ICW1_SINGLE	0x02		/* Single (cascade) mode */
+#define ICW1_INTERVAL4	0x04		/* Call address interval 4 (8) */
+#define ICW1_LEVEL	0x08		/* Level triggered (edge) mode */
+#define ICW1_INIT	0x10		/* Initialization - required! */
+#define ICW4_8086	0x01		/* 8086/88 (MCS-80/85) mode */
+#define ICW4_AUTO	0x02		/* Auto (normal) EOI */
+#define ICW4_BUF_SLAVE	0x08		/* Buffered mode/slave */
+#define ICW4_BUF_MASTER	0x0C		/* Buffered mode/master */
+#define ICW4_SFNM	0x10		/* Special fully nested (not) */
+#define PIC_EOI		0x20		/* End-of-interrupt command code */
+#define PIC1		0x20		/* IO base address for master PIC */
+#define PIC2		0xA0		/* IO base address for slave PIC */
+#define PIC1_COMMAND	PIC1
+#define PIC1_DATA	(PIC1+1)
+#define PIC2_COMMAND	PIC2
+#define PIC2_DATA	(PIC2+1)
+#define PIC1_CMD                    0x20
+#define PIC1_DATA                   0x21
+#define PIC2_CMD                    0xA0
+#define PIC2_DATA                   0xA1
+#define PIC_READ_IRR                0x0a    /* OCW3 irq ready next CMD read */
+#define PIC_READ_ISR                0x0b    /* OCW3 irq service next CMD read */
+
+#include <stdint.h>
+#include "io.h"
+
+enum KEYCODE {
+	NULL_KEY = 0,
+	Q_PRESSED = 0x10,
+	Q_RELEASED = 0x90,
+	W_PRESSED = 0x11,
+	W_RELEASED = 0x91,
+	E_PRESSED = 0x12,
+	E_RELEASED = 0x92,
+	R_PRESSED = 0x13,
+	R_RELEASED = 0x93,
+	T_PRESSED = 0x14,
+	T_RELEASED = 0x94,
+	Z_PRESSED = 0x15,
+	Z_RELEASED = 0x95,
+	U_PRESSED = 0x16,
+	U_RELEASED = 0x96,
+	I_PRESSED = 0x17,
+	I_RELEASED = 0x97,
+	O_PRESSED = 0x18,
+	O_RELEASED = 0x98,
+	P_PRESSED = 0x19,
+	P_RELEASED = 0x99,
+	A_PRESSED = 0x1E,
+	A_RELEASED = 0x9E,
+	S_PRESSED = 0x1F,
+	S_RELEASED = 0x9F,
+	D_PRESSED = 0x20,
+	D_RELEASED = 0xA0,
+	F_PRESSED = 0x21,
+	F_RELEASED = 0xA1,
+	G_PRESSED = 0x22,
+	G_RELEASED = 0xA2,
+	H_PRESSED = 0x23,
+	H_RELEASED = 0xA3,
+	J_PRESSED = 0x24,
+	J_RELEASED = 0xA4,
+	K_PRESSED = 0x25,
+	K_RELEASED = 0xA5,
+	L_PRESSED = 0x26,
+	L_RELEASED = 0xA6,
+	Y_PRESSED = 0x2C,
+	Y_RELEASED = 0xAC,
+	X_PRESSED = 0x2D,
+	X_RELEASED = 0xAD,
+	C_PRESSED = 0x2E,
+	C_RELEASED = 0xAE,
+	V_PRESSED = 0x2F,
+	V_RELEASED = 0xAF,
+	B_PRESSED = 0x30,
+	B_RELEASED = 0xB0,
+	N_PRESSED = 0x31,
+	N_RELEASED = 0xB1,
+	M_PRESSED = 0x32,
+	M_RELEASED = 0xB2,
+
+	ZERO_PRESSED = 0x29,
+	ONE_PRESSED = 0x2,
+	NINE_PRESSED = 0xA,
+
+	POINT_PRESSED = 0x34,
+	POINT_RELEASED = 0xB4,
+
+	SLASH_RELEASED = 0xB5,
+
+	BACKSPACE_PRESSED = 0xE,
+	BACKSPACE_RELEASED = 0x8E,
+	SPACE_PRESSED = 0x39,
+	SPACE_RELEASED = 0xB9,
+	ENTER_PRESSED = 0x1C,
+	ENTER_RELEASED = 0x9C,
+
+};
+
+
+void PIC_sendEOI(unsigned char irq)
+{
+	if(irq >= 8)
+		outb(PIC2_COMMAND,PIC_EOI);
+ 
+	outb(PIC1_COMMAND,PIC_EOI);
+}
+
+ 
+/*
+arguments:
+	offset1 - vector offset for master PIC
+		vectors on the master become offset1..offset1+7
+	offset2 - same for slave PIC: offset2..offset2+7
+*/
+void PIC_remap(int offset1, int offset2)
+{
+	unsigned char a1, a2;
+ 
+	a1 = inb(PIC1_DATA);                        // save masks
+	a2 = inb(PIC2_DATA);
+ 
+	outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+	io_wait();
+	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+	io_wait();
+	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
+	io_wait();
+	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
+	io_wait();
+	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+	io_wait();
+	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
+	io_wait();
+ 
+	outb(PIC1_DATA, ICW4_8086);
+	io_wait();
+	outb(PIC2_DATA, ICW4_8086);
+	io_wait();
+ 
+	outb(PIC1_DATA, a1);   // restore saved masks.
+	outb(PIC2_DATA, a2);
+}
+
+void IRQ_set_mask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+ 
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);        
+}
+ 
+void IRQ_clear_mask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+ 
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);        
+}
+ 
+/* Helper func */
+static uint16_t __pic_get_irq_reg(int ocw3)
+{
+    /* OCW3 to PIC CMD to get the register values.  PIC2 is chained, and
+     * represents IRQs 8-15.  PIC1 is IRQs 0-7, with 2 being the chain */
+    outb(PIC1_CMD, ocw3);
+    outb(PIC2_CMD, ocw3);
+    return (inb(PIC2_CMD) << 8) | inb(PIC1_CMD);
+}
+ 
+/* Returns the combined value of the cascaded PICs irq request register */
+uint16_t pic_get_irr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_IRR);
+}
+ 
+/* Returns the combined value of the cascaded PICs in-service register */
+uint16_t pic_get_isr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_ISR);
+}
+
+static char* _qwertzuiop = "qwertzuiop"; // 0x10-0x1c
+static char* _asdfghjkl = "asdfghjkl";
+static char* _yxcvbnm = "yxcvbnm";
+static char* _num = "123456789";
+uint8_t keyboard_to_ascii(uint8_t key)
+{
+    if(key == 0x1C) return '\n';
+    if(key == 0x39) return ' ';
+    if(key == 0xE) return '\r';
+    if(key == POINT_RELEASED) return '.';
+    if(key == SLASH_RELEASED) return '/';
+    if(key == ZERO_PRESSED) return '0';
+    if(key >= ONE_PRESSED && key <= NINE_PRESSED)
+        return _num[key - ONE_PRESSED];
+    if(key >= 0x10 && key <= 0x1C)
+    {
+        return _qwertzuiop[key - 0x10];
+    } else if(key >= 0x1E && key <= 0x26)
+    {
+        return _asdfghjkl[key - 0x1E];
+    } else if(key >= 0x2C && key <= 0x32)
+    {
+        return _yxcvbnm[key - 0x2C];
+    }
+    return 0;
+}
+
